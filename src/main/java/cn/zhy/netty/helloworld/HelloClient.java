71cb218fe36951cf6b0d6ba5523765c32db36516
zhengyang <zhy_justdoit@163.com>
2023-04-23 20:38:33 +0800

    add hello world servre

---
+++ src/main/java/cn/zhy/netty/helloworld/HelloClient.java
@@ -0,0 +1,30 @@
+package cn.zhy.netty.helloworld;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelloClient {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost",8089))
+                .sync()
+                .channel()
+                .writeAndFlush("hello,world");
+    }
+
+}
